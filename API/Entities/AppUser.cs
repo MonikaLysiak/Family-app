499142dbdf7b8f4ccc5453ce0c96e24f5de066ba
Monika Lysiak <monika@geo.pl>
2024-07-14 14:24:09 +0200

    'like user' feature

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,4 @@
-﻿using API.Extensions;
-
-namespace API.Entities;
+﻿namespace API.Entities;
 
 public class AppUser
 {
@@ -18,10 +16,8 @@ public class AppUser
     public string Interests { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public List<Photo> Photos { get; set; } = new();
+    public List<Photo> Photos { get; set; } = [];
 
-    // public int GetAge()
-    // {
-    //     return DateOfBirth.CalculateAge();
-    // }
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
 }
