286b3fab4cd1390468a12d40e0fdab6c271afc1f
Monika Lysiak <monika@geo.pl>
2024-07-23 23:17:34 +0200

    Identity and admin functionality

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.Entities;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -33,8 +35,10 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetService<UserManager<AppUser>>();
+    var roleManager = services.GetService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedUsers(context);
+    await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
 {
